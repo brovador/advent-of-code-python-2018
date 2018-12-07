41511d917d31595a79eef13d189d7deee9ab1461
Jesús Bartús Sancho <jesus.bartus@gmail.com>
2018-12-07 15:19:38 +0100

    day04 cleared! ⭐️

---
+++ day04/main2.py
@@ -0,0 +1,64 @@
+#encoding: utf-8
+import os
+import re
+import operator
+from collections import Counter
+from datetime import datetime
+from random import shuffle
+
+def main():
+	input_file = './input.txt'
+
+	with open(input_file, 'r') as f:
+		lines = [l.strip() for l in f]
+
+	r = re.compile(r'\[(.+)\] (.+)')
+	instructions = []
+	for l in lines:
+		d, i = r.match(l).groups()
+		d = datetime.strptime(d, '%Y-%m-%d %H:%M')
+		instructions.append((d, i))
+	instructions = sorted(instructions, key = lambda i: i[0])
+
+	current_guard = None
+	sleeping = False
+	last_d = None
+	sleeping_minutes = {}
+	
+	for ins in instructions:
+		d, i = ins
+		if last_d and sleeping:
+			mins = int((d - last_d).total_seconds() / 60)
+			for m in range(last_d.minute, last_d.minute + mins):
+				sleeping_minutes.setdefault(current_guard, []).append(m)
+		last_d = d
+		if 'shift' in i:
+			current_guard = int(re.match('Guard #(\d+) begins shift', i).groups()[0])
+			sleeping = False
+		elif 'wakes up' in i:
+			sleeping = False
+		elif 'asleep' in i:
+			sleeping = True
+	
+	sleeping_minutes = { k: Counter(v).most_common(1) for k, v in sleeping_minutes.items() }
+	sleeping_minutes = max(sleeping_minutes.items(), key = lambda kv: kv[1][0][1])
+	print sleeping_minutes[0] * sleeping_minutes[1][0][0]
+
+
+		
+
+	
+	
+	
+	
+
+
+		
+
+
+	
+	
+
+
+if __name__ == '__main__':
+	main()
